cee80891ce023eba8a8a21af0e185874f6547dad
Theodore Work Profile <theodore@digicom.et>
2025-09-23 13:23:33 +0300

    Your commit message here

---
+++ content-collections.ts
@@ -0,0 +1,38 @@
+import { extractMetadata } from '@/lib/extract-metadata'
+import { defineCollection, defineConfig } from '@content-collections/core'
+import { compileMDX } from '@content-collections/mdx'
+import { remarkPlugins } from '@prose-ui/core'
+
+const pages = defineCollection({
+  name: 'pages',
+  directory: 'content/docs',
+  include: '**/*.mdx',
+  schema: (z) => ({
+    title: z.optional(z.string()),
+  }),
+  transform: async (page, ctx) => {
+    const { toc, title } = await extractMetadata(page.content)
+    const content = await compileMDX(ctx, page, {
+      remarkPlugins: remarkPlugins(),
+    })
+    let path
+    if (page._meta.path === 'index') {
+      path = ''
+    } else if (page._meta.path.endsWith('/index')) {
+      path = page._meta.path.slice(0, -6)
+    } else {
+      path = page._meta.path
+    }
+    return {
+      ...page,
+      path: `/${path}`,
+      toc,
+      title: page.title ?? title,
+      content,
+    }
+  },
+})
+
+export default defineConfig({
+  collections: [pages],
+})
